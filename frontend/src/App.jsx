6b22c0fe7b39ef590bbf6c7f8a6c2f3802604947
anant-c <canant448@gmail.com>
2025-08-11 20:51:48 +0530

    nav changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Navigate to="/signin" />} />
         <Route path="/dashboard" element={<Suspense fallback={<Loader />}><Dashboard /></Suspense>} />
         <Route path="/signup" element={<Suspense fallback={<Loader />}><Signup /></Suspense>} />
         <Route path="/signin" element={<Suspense fallback={<Loader />}><Signin /></Suspense>} />
